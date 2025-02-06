0e59f90ecb79ddd7c704347b47a5321b840dd9ff
Arsalan23c <160375018+anusbutt@users.noreply.github.com>
2025-02-06 15:35:20 +0500

    Update env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,15 +1,18 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-22'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+// export const dataset = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+// )
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+// export const projectId = assertValue(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+// )
+
+export const dataset = 'production'; // Hardcoded value for testing
+export const projectId = 'cvghk2d8'; // Hardcoded value for testing
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
